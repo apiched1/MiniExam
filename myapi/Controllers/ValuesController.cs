b46d264516ef4a58fe854fb4bc473abd7a357597
apiched1 <apiched_oh@hotmail.com>
2018-12-22 11:01:25 +0700

    create project , set api

---
+++ myapi/Controllers/ValuesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using myapi.Models;
+
+namespace myapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        public static List<Students> Students = new List<Students>();
+
+        [HttpPost]
+        public void AddStudent(Students id)
+        {
+            Students.Add(id);
+        }
+
+        [HttpGet]
+        public ActionResult<List<Students>> GetAllStudent()
+        {
+            return Students;
+        }
+
+        [HttpGet]
+        public ActionResult<Students> CheckGrade()
+        {
+            var student = new Students();
+            
+            
+
+            return student;
+        }
+
+
+
+    }
+}
